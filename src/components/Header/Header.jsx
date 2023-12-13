942e241e63741b04f34f6c45d919d46a4e36f546
InnaTereschenko <120989288+InnaTereschenko@users.noreply.github.com>
2023-12-13 23:08:18 +0200

    header and toggle theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,25 @@
+import { useState, useEffect } from "react";
 import { HeaderStyle, WrapperHeader, Title, ThemeTogle } from "./Header.styled";
 import Container from "../Container/Container";
-import { IoMoon } from "react-icons/io5";
+import { IoMoon, IoMoonOutline } from "react-icons/io5";
 
 const Header = () => {
+    const [theme, setTheme] = useState('light');
+    
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme)
+    }, [theme]);
+
+    const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light');
+
   return (
     <HeaderStyle>
       <Container>
         <WrapperHeader>
           <Title>Where is the country?</Title>
-          <ThemeTogle>
-            <IoMoon /> Light Theme
+          <ThemeTogle onClick={toggleTheme}>
+                       {theme === 'light' ? (<IoMoonOutline size='14px'/>) : (<IoMoon size='14px'/>)}
+                      <span style={{marginLeft:'0.75rem'}}>{theme} Theme</span>
           </ThemeTogle>
         </WrapperHeader>
       </Container>
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -4,16 +4,29 @@ import styled from "styled-components";
 export const HeaderStyle = styled.header`
 box-shadow: var(--shadow);
 background-color: var(--color-ui-base);
+
 `
 
 export const WrapperHeader = styled.div`
-
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 2rem 0;
 `
 
-export const Title = styled.a``
+export const Title = styled.a`
+color: var(--color-text);
+font-size: var(--font-s-md);
+font-weight: var(--font-w-bold);
+`
     // .attrs({ to: '/' })`
 
 // `
 export const ThemeTogle = styled.div`
-
+text-align: center;
+cursor: pointer;
+color: var(--color-text);
+font-size: var(--font-s-md);
+font-weight: var(--font-w-bold);
+text-transform: capitalize;
 `
